734fccdb39807b7e0a52c51460d4cdbe7cf84bd9
Dartus <petr@radulay.eu>
2024-02-03 21:40:54 +0100

    Update fps_bot.py

--- fps_bot.py
+++ fps_bot.py
@@ -10,6 +10,7 @@ TARGET_CHANNEL_ID = TARGET_CHANNEL_ID
 # Replace 'TARGET_FOLDER' with the path to the folder where you want to save the files
 TARGET_FOLDER = 'X:\\Your\\Target\\folder'
 
+
 intents = discord.Intents.default()
 intents.messages = True
 intents.message_content = True
@@ -25,7 +26,7 @@ async def on_ready():
     # Get the target channel
     target_channel = bot.get_channel(TARGET_CHANNEL_ID)
     
-    # Send a message to the target channel upon bot connection
+    # Send a message to the target channel
     await target_channel.send('**FPS Bot**, se připojil! Nyní můžete nahrávat soubory s misemi.')
 
 @bot.event
@@ -41,7 +42,8 @@ async def on_message(message):
         # print(f'Detected new message with content: {message.content}')
 
         for attachment in message.attachments:
-            await save_attachment(attachment)
+            # Pass the uploader's name to the save_attachment function
+            await save_attachment(attachment, message.author.id)
 
             # Add a reaction after saving the attachment
             await message.add_reaction("✅")
@@ -49,7 +51,7 @@ async def on_message(message):
     await bot.process_commands(message)
 
 
-async def save_attachment(attachment):
+async def save_attachment(attachment, uploader_name):
     file_name = attachment.filename
     file_path = os.path.join(TARGET_FOLDER, file_name)
 
@@ -61,9 +63,9 @@ async def save_attachment(attachment):
     print(f'Saved file: {file_name}')
     print(f'File size: {len(content)} bytes')
 
-    # Send a message to Discord with the saved filename after it was processed
+    # Send a message to channel with mention of uploader and the saved filename after it was processed
     channel = bot.get_channel(TARGET_CHANNEL_ID)
-    await channel.send(f"Mission file **__'{file_name}'__** byl úspěšně nahrán!")
+    await channel.send(f"<@{uploader_name}>, Mission file **__'{file_name}'__** byl úspěšně nahrán!")
     await channel.send(f"**---------------------- Nyní můžete nahrát další soubor ----------------------**")
 
 
